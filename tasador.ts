ffc7c2bcbc86d64fd04378d92a8f56cb49c7b2f2
Dracorfs <franciscosantopietro@hotmail.com>
2025-06-22 18:11:52 -0300

    add data from isolated json

---
+++ tasador.ts
@@ -0,0 +1,25 @@
+import get_location_value from "./method/location.ts"
+import data from "./data.json" with { type: "json" }
+//Precio de cierre m2 de CABA (en USD ajustado por inflacion EEUU comienzo 2025)
+//recurso: usinflationcalculator.com/inflation/calculator-cumulative/
+
+const today = new Date()
+let precio_promedio_cierre = data.precios_promedio_cierre[today.getFullYear()]
+console.log(`Precio promedio de cierre: ${precio_promedio_cierre} ${data.unidad}.`)
+
+// Metodo 3 6 9 12
+let value = precio_promedio_cierre
+
+const location_value = get_location_value(['parques'], 'mediana', 'bueno')
+console.log(location_value)
+
+value = precio_promedio_cierre * location_value
+console.log(`Valor considerando la locacion (Indice=${location_value}): ${value} ${data.unidad}.`)
+
+// URL
+const precio_base = 20000
+const precio_techo = 200000
+const parameter = `-${precio_base}-${precio_techo}-dolar`
+const orden = `-orden-precio-m2-ascendente`
+const url_base = `https://www.zonaprop.com.ar/departamentos-venta${parameter+orden}.html`
+console.log(url_base)
