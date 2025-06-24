13663ace2c9a75151a504da682e6e14b330027d7
Dracorfs <franciscosantopietro@hotmail.com>
2025-06-24 20:15:42 -0300

    add first API on the edge example

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 deno run ./tasador.js
+
+Ejemplo de request:
+POST
+https://api-tasador-baires.deno.dev/tasacion
+{
+    "vecinos": [
+        "parques"
+    ],
+    "iluminacion": "buena",
+    "arbolado": "bueno",
+    "edad": 15,
+    "calidad": "medium",
+    "espacios_comunes": "good",
+    "servicios_centrales": "medium",
+    "exteriores": "good",
+    "inmutable": {
+        "cubiertos": 70,
+        "semiCubiertos": 5,
+        "descubiertos_balcon": 5,
+        "descubiertos_patio": 0,
+        "expensas": 25000,
+        "impuestos_fijos": 10000,
+        "piso": 6,
+        "altura_total_edificio": 10,
+        "orientacion": "NE",
+        "disposicion": "frente",
+        "tipo": "duplex"
+    }
+}
--- api.ts
+++ api.ts
@@ -4,35 +4,6 @@ import get_building_value from "./methods/building.ts"
 import get_inmutable_value from "./methods/inmutable.ts"
 import data from "./data.json" with { type: "json" }
 
-/*
-Ejemplo de request
-{
-    "vecinos": [
-        "parques"
-    ],
-    "iluminacion": "buena",
-    "arbolado": "bueno",
-    "edad": 15,
-    "calidad": "medium",
-    "espacios_comunes": "good",
-    "servicios_centrales": "medium",
-    "exteriores": "good",
-    "inmutable": {
-        "cubiertos": 70,
-        "semiCubiertos": 5,
-        "descubiertos_balcon": 5,
-        "descubiertos_patio": 0,
-        "expensas": 25000,
-        "impuestos_fijos": 10000,
-        "piso": 6,
-        "altura_total_edificio": 10,
-        "orientacion": "NE",
-        "disposicion": "frente",
-        "tipo": "duplex"
-    }
-}
-*/
-
 serve(async (req: Request) => {
 	if (req.method === "POST" && new URL(req.url).pathname === "/tasacion") {
 		const body = await req.json()
