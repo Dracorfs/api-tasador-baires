9dada91a99f70e8ad708894ed2a61bec5d026d75
Dracorfs <franciscosantopietro@hotmail.com>
2025-06-26 20:40:57 -0300

    fix language in location method

--- methods/location.ts
+++ methods/location.ts
@@ -1,37 +1,37 @@
 /*
-3 variables de ubicacion
-	1. Vecinos especiales:
-		Restan absoluto 20%: Bomberos, autopistas, boliches, albergues transitorios, trenes, funerarias, hospitales, zonas rojas.
-		Pueden sumar positivamente: parques, plazas, clubes.
-	2. Iluminacion artificial en la cuadra: buena, mala, mediana.
-	3. Arbolado de la cuadra: bueno, malo, mediano.
-Ponderacion total: +-20%
+3 location variables:
+	1. Special neighbours:
+		Absolute -20%: Bomberos, autopistas, boliches, albergues transitorios, trenes, funerarias, hospitales, zonas rojas.
+		Might add value: parques, plazas, clubes.
+	2. Artificial lighting of the block.
+	3. Greenery.
+Total: +-20%
 */
-export type Iluminacion = 'buena' | 'mediana' | 'mala'
-export type Arbolado = 'bueno' | 'mediano' | 'malo'
+export type Quality = 'good' | 'medium' | 'bad'
+export type Neighbours = string[]
 
 export default function get_location_value(
-	vecinos: string[],
-	iluminacion: Iluminacion,
-	arbolado: Arbolado
+	neighbours: Neighbours,
+	lighting: Quality,
+	greenery: Quality
 ): number {
 	let ponderacion_total = 1
-	let vecinos_valoracion = verify_neighbours(vecinos)
+	const vecinos_valoracion = verify_neighbours(neighbours)
 
-	if (vecinos_valoracion === 'negativo') {
+	if (vecinos_valoracion === 'negative') {
 		ponderacion_total = 0.8
 	}
-	else if (iluminacion !== 'mala' && arbolado !== 'malo') {
+	else if (lighting !== 'bad' && greenery !== 'bad') {
 		ponderacion_total = 1.2
 	}
 
 	return ponderacion_total
 }
-function verify_neighbours(neighbour_list) {
+function verify_neighbours(neighbour_list: Neighbours) {
 	const negative_neighbours = ['bomberos', 'autopistas', 'boliches', 'albergues transitorios', 'trenes', 'funerarias', 'hospitales', 'zonas rojas']
 	const positive_neighbours = ['parques', 'plazas', 'clubes']
 
-	if (neighbour_list.some(neighbour => negative_neighbours.includes(neighbour))) return 'negativo'
-	if (neighbour_list.some(neighbour => positive_neighbours.includes(neighbour))) return 'positivo'
+	if (neighbour_list.some(neighbour => negative_neighbours.includes(neighbour))) return 'negative'
+	if (neighbour_list.some(neighbour => positive_neighbours.includes(neighbour))) return 'positive'
 	return 'neutro'
 }
