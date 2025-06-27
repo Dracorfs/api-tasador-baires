fb2867a2d294bea86df30e7ee3a40d7a13d05999
Dracorfs <franciscosantopietro@hotmail.com>
2025-06-26 21:43:24 -0300

    add condition method

---
+++ methods/condition.ts
@@ -0,0 +1,22 @@
+/*
+Condition-related variables (can be improved)
+(Overall weight: ±20%. So if fully renovated: +50%)
+    Kitchen
+    Bathrooms
+    Walls
+    Floors
+    Closets
+Installations:
+    Water supply
+    Gas system
+    Electrical system
+    Drainage functionality
+Systems:
+    Heating
+    Cooling / Air conditioning
+    Ventilation
+*/
+
+export default function get_condition_value() {
+    
+}
